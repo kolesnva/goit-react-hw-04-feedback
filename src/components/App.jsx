909e7f0343deaac75baf4c8930b97dff89a1eba4
kolesnva <kolesnychenko_v@icloud.com>
2022-08-16 09:46:29 +0300

    done step 1 and 2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,54 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+
+export class App extends Component {
+  state = {
+    good: 3,
+    neutral: 2,
+    bad: 2,
+  };
+
+  countTotalFeedback = () => {
+    const { good, neutral, bad } = this.state;
+    const total = good + neutral + bad;
+    return total;
+  };
+
+  countPositiveFeedbackPercentage = total => {
+    const { good } = this.state;
+
+    if (total === 0) {
+      return 0;
+    }
+
+    const positivePercentage = (good * 100) / total;
+
+    return Math.round(positivePercentage);
+  };
+
+  render() {
+    const total = this.countTotalFeedback();
+    const { good, neutral, bad } = this.state;
+    return (
+      <>
+        <h2>Please leave feedback</h2>
+        <div>
+          <button>Good</button>
+          <button>Neutral</button>
+          <button>Bad</button>
+        </div>
+
+        <div>
+          <h2>Statistics</h2>
+          <p>Good: {good}</p>
+          <p>Neutral: {neutral}</p>
+          <p>Bad: {bad}</p>
+          <p>Total: {total}</p>
+          <p>
+            Positive feedback:{' '}
+            {this.countPositiveFeedbackPercentage(total) + `%`}
+          </p>
+        </div>
+      </>
+    );
+  }
+}
